afb296eee3f27297b01aaf8d775dc9c30070537a
Ryo Tsukahara <tsukahararu@gmail.com>
2021-04-16 15:10:58 +0900

    update

--- mod.tsx
+++ mod.tsx
@@ -2,16 +2,16 @@ interface Responder {
   respondWith(res: Response): void;
 }
 
-const body = (
-    <div>
-    Hello
-    </div>
-)
+// const body = (
+//     <div>
+//     Hello
+//     </div>
+// )
 
 addEventListener("fetch", (event) => {
   const e = (event as unknown) as Responder;
   e.respondWith(
-    new Response(body, {
+    new Response("body", {
       status: 200,
       headers: {
         server: "denosr",
