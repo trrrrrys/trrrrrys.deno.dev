f612d2cf91e6bd4456ded7c7bdf4bec7b6694c71
Ryo Tsukahara <tsukahararu@gmail.com>
2021-04-16 15:04:52 +0900

    updat

---
+++ mod.tsx
@@ -0,0 +1,22 @@
+interface Responder {
+  respondWith(res: Response): void;
+}
+
+const body = (
+    <div>
+    Hello
+    </div>
+)
+
+addEventListener("fetch", (event) => {
+  const e = (event as unknown) as Responder;
+  e.respondWith(
+    new Response(body, {
+      status: 200,
+      headers: {
+        server: "denosr",
+        "content-type": "text/html",
+      },
+    }),
+  );
+});
